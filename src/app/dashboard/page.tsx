a32c07de0ef655c809d6646303e88cadff1e6e36
Pedro Gomes <pedrocgomes@proton.me>
2023-07-01 15:36:47 -0400

    update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,33 @@
 'use client'
 
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { ChangeEvent, FormEvent, useEffect, useMemo, useState } from 'react'
 import { useSession } from 'next-auth/react'
+import { ITask } from '@/types/dbTypes'
+import Database from '@/controller/DatabaseController'
+
 import { db } from '@/services/firebaseConnection'
 import { addDoc, collection } from 'firebase/firestore'
-import Head from 'next/head'
-import styles from './styles.module.css'
 
+import Link from 'next/link'
+import Head from 'next/head'
 import { TextArea } from '@/components/TextArea'
 import { FiShare2, FiTrash } from 'react-icons/fi'
+import styles from './styles.module.css'
 
 export default function Dashboard() {
-  // eslint-disable-next-line no-unused-vars
   const { data: session } = useSession({
     required: true,
   })
-
-  const [task, setTask] = useState<string | null>('')
+  const [data, setData] = useState<ITask[]>([] as ITask[])
+  const [task, setTask] = useState<string | null | undefined>('')
   const [isPublic, setIsPublic] = useState<boolean>(false)
+  const database = useMemo(() => new Database(), [])
 
   async function handleNewTask(e: FormEvent) {
     e.preventDefault()
     if (!task) {
       alert('Type a new task')
     }
-
     try {
       await addDoc(collection(db, 'tasks'), {
         task,
@@ -40,7 +43,29 @@ export default function Dashboard() {
     }
   }
 
-  useEffect(() => {}, [])
+  async function handleShare(id: string) {
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_URL}/task/${id}`,
+    )
+    alert('Link copied to clipboard')
+  }
+
+  async function handleDelete(id: string) {
+    await database.deleteTask(id)
+    alert('Task deleted!')
+  }
+
+  useEffect(() => {
+    async function fetchTasks() {
+      try {
+        const dbRes = await database.loadUserTasks(session!.user!.email)
+        setData(dbRes)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchTasks()
+  }, [data, database, session])
   return (
     <div className={styles.container}>
       <Head>
@@ -75,22 +100,39 @@ export default function Dashboard() {
         <section className={styles.taskWrapper}>
           <h1>My Tasks</h1>
 
-          <article className={styles.task}>
-            <div className={styles.tagWrapper}>
-              <label htmlFor="" className={styles.tag}>
-                Public
-              </label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
-            <div className={styles.taskContent}>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <button className={styles.trashButton}>
-                <FiTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+          {data &&
+            data.map((task) => (
+              <article className={styles.task} key={task.id}>
+                {task.public === true && (
+                  <div className={styles.tagWrapper}>
+                    <label htmlFor="" className={styles.tag}>
+                      Public
+                    </label>
+                    <button
+                      className={styles.shareButton}
+                      onClick={() => handleShare(task.id)}
+                    >
+                      <FiShare2 size={22} color="#3183ff" />
+                    </button>
+                  </div>
+                )}
+                <div className={styles.taskContent}>
+                  {task.public === true ? (
+                    <Link href={`/task/${task.id}`}>
+                      <p>{task.task}</p>
+                    </Link>
+                  ) : (
+                    <p>{task.task}</p>
+                  )}
+                  <button
+                    className={styles.trashButton}
+                    onClick={() => handleDelete(task.id)}
+                  >
+                    <FiTrash size={24} color="#ea3140" />
+                  </button>
+                </div>
+              </article>
+            ))}
         </section>
       </main>
     </div>
